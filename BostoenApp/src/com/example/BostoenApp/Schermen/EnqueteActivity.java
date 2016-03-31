8d0c852cccd3cf009e316066df4f46e93a625515
Marnix Hauspie <r0483971@student.vives.be>
2016-03-31 15:49:19 +0200

    radiobutton aanpassing

--- BostoenApp/src/com/example/BostoenApp/Schermen/EnqueteActivity.java
+++ BostoenApp/src/com/example/BostoenApp/Schermen/EnqueteActivity.java
@@ -3,17 +3,38 @@ package com.example.BostoenApp.Schermen;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.BostoenApp.R;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by Marnix on 20/03/2016.
  */
 public class EnqueteActivity extends Activity  implements FragmentsInterface{
+    private ArrayAdapter<String> adapter;
+    private List<String> list;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.enquete_layout);
+
+        ListView drawer =(ListView)findViewById(R.id.left_drawer);
+
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2" };
+        list = new ArrayList<String>();
+        Collections.addAll(list, values);
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, list);
+        drawer.setAdapter(adapter);
     }
 
     @Override
